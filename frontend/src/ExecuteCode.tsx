7a8aeb7b6223fe57a925969d65dc44eb324ab345
Adedoyin Adepetun <aaa245@student.bham.ac.uk>
2023-07-21 17:58:07 +0100

    setting up judge0 api

--- frontend/src/ExecuteCode.tsx
+++ frontend/src/ExecuteCode.tsx
@@ -1,38 +1,38 @@
-import axios from 'axios';
-import {Button} from "@chakra-ui/react";
-
-
-function ExecuteCode() {
-    const PISTON_API_URL = 'https://emkc.org/api/v1/piston/execute';
-    const LANGUAGE = 'typescript';
-    const CODE = `
-  function add(a: number, b: number): number {
-    return a + b;
-  }
-  console.log(add(5, 3));
-`;
-
-    const executeCode = async () => {
-        try {
-            const response = await axios.post(PISTON_API_URL, {
-                language: LANGUAGE,
-                version: 'latest',
-            });
-
-            if (response.data.run?.stdout) {
-                console.log('Output:', response.data.run.stdout);
-            } else {
-                console.log('Error:', response.data.run?.stderr);
-            }
-        } catch (error) {
-            console.error('Error:', error.response?.data);
-        }
-    };
-
-
-    return (
-        <Button onClick={executeCode}>click me</Button>
-        )
-
-}
-export default ExecuteCode
+// import axios from 'axios';
+// import {Button} from "@chakra-ui/react";
+//
+//
+// function ExecuteCode() {
+//     const PISTON_API_URL = 'https://emkc.org/api/v1/piston/execute';
+//     const LANGUAGE = 'typescript';
+//     const CODE = `
+//   function add(a: number, b: number): number {
+//     return a + b;
+//   }
+//   console.log(add(5, 3));
+// `;
+//
+//     const executeCode = async () => {
+//         try {
+//             const response = await axios.post(PISTON_API_URL, {
+//                 language: LANGUAGE,
+//                 version: 'latest',
+//             });
+//
+//             if (response.data.run?.stdout) {
+//                 console.log('Output:', response.data.run.stdout);
+//             } else {
+//                 console.log('Error:', response.data.run?.stderr);
+//             }
+//         } catch (error) {
+//             console.error('Error:', error.response?.data);
+//         }
+//     };
+//
+//
+//     return (
+//         <Button onClick={executeCode}>click me</Button>
+//         )
+//
+// }
+// export default ExecuteCode
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client'
-// import {ChakraProvider} from "@chakra-ui/react";
-// import theme from "./theme.ts";
-// import App from "./App.tsx";
-import ExecuteCode from "./ExecuteCode.tsx";
+import {ChakraProvider} from "@chakra-ui/react";
+import theme from "./theme.ts";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ExecuteCode/>
+    <ChakraProvider theme={theme}>
+        <App/>
+    </ChakraProvider>
 )
