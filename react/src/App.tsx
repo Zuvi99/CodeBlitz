ca997a68ead0644c8838c39c6d3c5cd72bbec5ef
Adedoyin Adepetun <62803035+Zuvi99@users.noreply.github.com>
2023-08-29 14:34:06 +0100

    Update App.tsx

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -209,6 +209,7 @@ function App() {
                                                                 changeMenuLanguage("Python");
                                                                 clearTextArea();
                                                                 clearOutputCheckMessage();
+                                                                defaultPlaceholder();
                                                             }}
                                                             value={"python"}
                                                         >
